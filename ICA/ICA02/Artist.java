8c95074e6428a7ccd6dccc4cb2cb1be6bc290ce1
mayayr115 <mayaramkishuncsh2027@gmail.com>
2026-09-14 10:31:58 -0400

    Completed ICA02

--- ICA/ICA02/Artist.java
+++ ICA/ICA02/Artist.java
@@ -7,22 +7,29 @@ public class Artist {
 
     // No-argument constructor
     public Artist() {
-        //Add code here
+        this.name = "Unknown";
+        this.numFans = 0;
+        this.albums = new String[0];
     }
 
     // Parameterized constructor
     public Artist(String name, int numFans, String[] albums) {
-        //Add code here
+        setInfo(name, numFans, albums);
     }
 
     // Copy constructor (deep copy)
     public Artist(Artist anotherArtist) {
-        //Add code here
+        setInfo(anotherArtist.name, anotherArtist.numFans, anotherArtist.albums);
     }
 
     // Method to set info , deep copy of albums array
     public void setInfo(String name, int numFans, String[] albums) {
-        //Add code here
+        this.name = name;
+        this.numFans = numFans;
+        this.albums = new String[albums.length];
+        for (int i = 0; i < albums.length; ++i) {
+            this.albums[i] = albums[i];
+        }
     }
 
     // Method to display artist info
